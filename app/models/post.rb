543653372b6899aee812ec61a5f9f4f5941e0fee
Hasumi-Uehara <slammygirl32@gmail.com>
2021-01-09 13:39:54 +0900

    都道府県のデータ・アソシエーション・バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
   belongs_to :user
   has_one_attached :image
   
@@ -7,7 +10,6 @@ class Post < ApplicationRecord
     validates :event_date
     validates :user_id
     validates :image
+    validates :prefecture_id, numericality: { other_than: 0 }
   end
-
-  validates :prefecture_id, numericality: { other_than: 0 }
 end
