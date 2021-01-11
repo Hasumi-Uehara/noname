77a7cfe8028c0f1dc023833230010a8ff7f63e5f
Hasumi-Uehara <slammygirl32@gmail.com>
2021-01-11 15:41:18 +0900

    アソシエーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,6 @@ class Post < ApplicationRecord
   with_options presence: true do
     validates :event_name
     validates :event_date
-    validates :user_id
     validates :image
     validates :prefecture_id, numericality: { other_than: 0 }
   end
