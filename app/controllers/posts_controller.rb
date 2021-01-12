f8196ae1b5459f279c18f2071a76b53a8f5eb08b
Hasumi-Uehara <slammygirl32@gmail.com>
2021-01-12 17:13:40 +0900

    削除posts#deleteコントローラーの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index,      only: [:edit, :update]
   
   def index
     @posts = Post.includes(:user).order(created_at: 'DESC')
@@ -34,10 +35,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy if current_user.id == @post.user_id
+    redirect_to user_path(@post.user.id)
+  end
+
   private
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user.id == @post.user_id
   end
 
   def post_params
