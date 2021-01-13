fdf886204a17d08abc610dafab2b4eaeccdab0f7
Hasumi-Uehara <slammygirl32@gmail.com>
2021-01-13 20:43:41 +0900

    ホーム画面のルーティング設定/ルートパス変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'posts#index'
+  root to: 'homes#index'
+  post '/homes/guest_sign_in', to: 'homes#new_guest'
+  resources :homes, only: [:index]
   resources :posts
   resources :users, only: [:show, :edit, :update]
 end
