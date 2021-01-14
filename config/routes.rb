ff7f772b6ed985e603e71a695a3aaea7b00ce26e
Hasumi-Uehara <slammygirl32@gmail.com>
2021-01-14 21:31:20 +0900

    searchルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
+
   post '/homes/guest_sign_in', to: 'homes#new_guest'
+  get '/posts/search', to: 'posts#search'
+  get '/posts/search_table',  to: 'posts#search_table'
   resources :homes, only: [:index]
-  resources :posts
+  resources :posts do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: [:show, :edit, :update]
 end
