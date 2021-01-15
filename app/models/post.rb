4f0257c3a073e9103e777ba69c719bc50a0b2fb7
Hasumi-Uehara <slammygirl32@gmail.com>
2021-01-15 21:56:42 +0900

    Likeモデルの生成・テーブル作成・アソシエーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,13 @@ class Post < ApplicationRecord
   belongs_to_active_hash :prefecture
 
   belongs_to :user
+  has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one_attached :image, dependent: :destroy
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
   
   with_options presence: true do
     validates :event_name
